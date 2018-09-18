16034197c978ba0f94d179f33d61537f6148574d
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2018-09-19 00:52:04 +0300

    navigation start

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,16 +3,33 @@ import styles from './navigation.scss';
 
 import Logo from '../logo/logo';
 
+const linksList = [
+    {
+        link: '/articles',
+        label: 'Articles'
+    },
+    {
+        link: '/contacts',
+        label: 'Contacts'
+    },
+    {
+        link: '/posts',
+        label: 'Posts'
+    }
+];
+
+
+
 function Navigation() {
     return (
         <nav className={styles.navigation}>
             <Logo/>
-            <ul>
-                <li>
-                    <a href="#">Menu item</a>
-                    <a href="#">Menu item</a>
-                    <a href="#">Menu item</a>
-                    <a href="#">Menu item</a>
+
+            <ul className={styles.navigation__menu}>
+                <li className={styles.navigation__item}>
+                    <a className={styles.navigation__link}>
+
+                    </a>
                 </li>
             </ul>
         </nav>
